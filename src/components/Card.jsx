c2e01bc483c6220e97574779b9a9a24505578f83
goodwin-christopher <goodwin.chris2021@gmail.com>
2023-05-31 17:03:00 -0500

    initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => {
+    return (
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow5'>
+            <img
+                src={`https://robohash.org/${id}`}
+                alt='robots'
+                srcSet=''
+            />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
