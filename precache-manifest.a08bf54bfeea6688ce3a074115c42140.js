580f0eb325e2a3666a33ada97eab04d2a2de0211
goodwin-christopher <goodwin.chris2021@gmail.com>
2023-05-31 19:04:57 -0500

    Updates

---
+++ precache-manifest.a08bf54bfeea6688ce3a074115c42140.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05faa0cdc79a71ae062",
+    "url": "goodwin.christopher.github.io/robofriends/static/css/main.93f79771.chunk.css"
+  },
+  {
+    "revision": "e05faa0cdc79a71ae062",
+    "url": "goodwin.christopher.github.io/robofriends/static/js/main.ee3888eb.chunk.js"
+  },
+  {
+    "revision": "bbe56c315021d4c2a9f8",
+    "url": "goodwin.christopher.github.io/robofriends/static/js/runtime~main.10d1fd05.js"
+  },
+  {
+    "revision": "964f914cab4ab98218a3",
+    "url": "goodwin.christopher.github.io/robofriends/static/css/2.647f08e0.chunk.css"
+  },
+  {
+    "revision": "964f914cab4ab98218a3",
+    "url": "goodwin.christopher.github.io/robofriends/static/js/2.39830d21.chunk.js"
+  },
+  {
+    "revision": "089bc876b02613239fa5",
+    "url": "goodwin.christopher.github.io/robofriends/static/js/3.b7c4cbb6.chunk.js"
+  },
+  {
+    "revision": "ecf427f7510fa5366337cefeeeb727d2",
+    "url": "goodwin.christopher.github.io/robofriends/static/media/MR ROBOT.ecf427f7.ttf"
+  },
+  {
+    "revision": "ed02b1942ef0251ec0cf3d565f365558",
+    "url": "goodwin.christopher.github.io/robofriends/index.html"
+  }
+];
